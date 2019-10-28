2cb2dfd39a9c65296416cde6fa1a063347b3e82b
starship-s <45587122+starship-s@users.noreply.github.com>
2019-10-28 13:23:34 -0600

    Script to grab all objects from Met museum API
    For loading in to database

---
+++ met_grabber.py
@@ -0,0 +1,25 @@
+import requests
+
+met = requests.get('https://collectionapi.metmuseum.org/public/collection/v1/objects', params='objectIDs')
+
+met = met.json()
+
+object_list = [k for k in met.values()]
+
+for art_object in object_list[1]:
+
+    url = 'https://collectionapi.metmuseum.org/public/collection/v1/objects/' + str(art_object)
+
+    obj_request = requests.get(url)
+
+    obj_request = obj_request.json()
+
+    print("Object ID: " + str(obj_request['objectID']))
+    print("Artist Name: " + obj_request['artistDisplayName'])
+    print("Title: " + obj_request['title'])
+    print("Image: " + obj_request['primaryImage'])
+    print("\n--------------------\n")
+
+
+
+
