05533ac5a4fe1e84923a620cb0df4af0f8dfd4f1
Justin <jmaughan@rocketmail.com>
2019-11-05 15:41:42 -0700

    Update to the JSON fields the script pulls

--- met_grabber.py
+++ met_grabber.py
@@ -17,13 +17,23 @@ for query in queries:
         obj_request = requests.get(url).json()
 
         # check to make sure item has all necessary parts to build web card, skip if it doesn't
-        if len(str(obj_request['objectID'])) > 0 and len(obj_request['artistDisplayName']) > 0 and \
-                len(obj_request['title']) > 0 and len(obj_request['primaryImage']) > 0:
+        if len(str(obj_request['objectID'])) > 0 \
+                and len(obj_request['artistDisplayName']) > 0 \
+                and len(obj_request['title']) > 0 \
+                and len(obj_request['primaryImage']) > 0 \
+                and obj_request['isPublicDomain']\
+                and len(obj_request['objectDate']) > 0\
+                and len(obj_request['medium']) > 0\
+                and len(obj_request['department']) > 0:
 
             print("Object ID: " + str(obj_request['objectID']))
             print("Artist Name: " + obj_request['artistDisplayName'])
             print("Title: " + obj_request['title'])
             print("Image: " + obj_request['primaryImage'])
+            print("Classification: " + obj_request['classification'])
+            print("Object Date: " + obj_request['objectDate'])
+            print("Object Medium: " + obj_request['medium'])
+            print("Department: " + obj_request['department'])
             print(query)
             print("\n--------------------\n")
 
